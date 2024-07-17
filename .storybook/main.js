836910090cdbcab63df168c1f7ed66d07e94b8bf
José Esteva <josesteva@soft4pilot.net>
2024-07-17 12:14:20 -0600

    Eliminé el complemento storybook-design-token que utilizaba para mostrar los vlores de las variables de diseño (Tokens). Prefiero utilizar la funcionalidad nativa de Storybook para presentar colores y tipografías.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,19 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
+
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
+    "@chromatic-com/storybook",
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    { name: 'storybook-design-token', options: { preserveCSSVars: true } }
   ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
 };
+
 export default config;
+
